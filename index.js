ef58a66b032b64fc7a3cc1dd929d52bff24c5746
unknown <saad.binabid@palmira-itc.com>
2024-02-24 16:57:30 +0500

    youtube video download form url

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import ytdl from "ytdl-core";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Welcome to ytdl App.");
+});
+
+app.get("/download", async (req, res) => {
+  try {
+    const url = req.query.url;
+    const info = await ytdl.getInfo(url);
+
+    const format = ytdl.chooseFormat(info.formats, { quality: "highest" });
+    const title = info.videoDetails.title;
+
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename*=UTF-8 ''${encodeURIComponent(title)}.mp4`
+    );
+    ytdl(url, { format }).pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
