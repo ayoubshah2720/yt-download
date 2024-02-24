ed200f05934b027711871acfef297107e6f5f24a
unknown <saad.binabid@palmira-itc.com>
2024-02-24 21:12:17 +0500

    get audio url in response from youtube video url

--- index.js
+++ index.js
@@ -11,11 +11,19 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
+const corsOptions = {
+  origin: 'http://localhost:4200/', //Front-end url.
+  credential: true,
+  optionSuccessStatus: 200,
+  exposedHeaders: '**',
+}
 app.get("/", (req, res) => {
   res.send("Welcome to ytdl App.");
 });
 
 app.get("/download", async (req, res) => {
+  // how to pass parameter in url
+  // http://localhost:3000/download?url=https://www.youtube.com/watch?v=D6ObIEU2UZ4
   try {
     const url = req.query.url;
     const info = await ytdl.getInfo(url);
@@ -35,6 +43,8 @@ app.get("/download", async (req, res) => {
 });
 
 app.get("/thumbnail", async (req, res) => {
+  // how to pass parameter in url
+  // http://localhost:3000/thumbnail?url=https://www.youtube.com/watch?v=D6ObIEU2UZ4
   try {
     const url = req.query.url;
     const info = await ytdl.getInfo(url);
@@ -46,10 +56,30 @@ app.get("/thumbnail", async (req, res) => {
 });
 
 app.get("/videos", async (req, res) => {
+  // how to pass parameter in url
+  // http://localhost:3000/videos?url=https://www.youtube.com/watch?v=D6ObIEU2UZ4
   try {
     const url = req.query.url;
     const info = await ytdl.getInfo(url);
     res.send(info.formats)
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+});
+
+
+app.get("/audio", async (req, res) => {
+  // how to pass parameter in url
+  // http://localhost:3000/download?url=https://www.youtube.com/watch?v=D6ObIEU2UZ4
+  try {
+    const url = req.query.url;
+    const info = await ytdl.getInfo(url);
+    const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+
+    audioFormats.map((item)=>{
+      res.send(item.url)
+    })
   } catch (error) {
     console.error(error);
     res.status(500).send(error.message);
