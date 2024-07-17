67a0c9bfecf3c8fec07b70fe4bd1e870c7d2ecea
Ayoub Shah <ayoubshah2720@gmail.com>
2024-07-17 12:42:48 +0500

    getting info against youtube url

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get("/download", async (req, res) => {
   // http://localhost:3000/download?url=https://www.youtube.com/watch?v=D6ObIEU2UZ4
   try {
     const url = req.query.url;
-    const videoQuality = req.query.type;
+    const videoQuality = req.query.type || '360p';
     const info = await ytdl.getInfo(url);
     // console.log('qualityqualityqualityquality',req.query);
     // console.log('infoinfoinfoinfoinfoinfoinfo',info.formats);
@@ -67,6 +67,30 @@ app.get("/download", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send(error.message);
+  }
+});
+
+app.get('/getVideoInfo', async (req, res) => {
+  const videoURL = req.query.url;
+
+  if (!videoURL) {
+      return res.status(400).send('URL parameter is required');
+  }
+
+  try {
+      const info = await ytdl.getInfo(videoURL);
+      const formats = ytdl.filterFormats(info.formats, 'video');
+
+      res.json({
+          title: info.videoDetails.title,
+          formats: formats.map(format => ({
+              quality: format.qualityLabel,
+              url: format.url
+          }))
+      });
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching video info');
   }
 });
 
@@ -113,6 +137,32 @@ app.get("/audio", async (req, res) => {
     res.status(500).send(error.message);
   }
 });
+
+app.get("/allVideos", async (req,res)=>{
+  try {
+    // Fetch video information
+    const url = req.query.url;
+    const info = await ytdl.getInfo(url);
+
+    // Extract thumbnail URL
+    const thumbnails = info.videoDetails.thumbnails;
+
+    // Extract available formats
+    const formats = ytdl.filterFormats(info.formats, 'videoandaudio');
+    formats.forEach(format => {
+      console.log(`${format.qualityLabel}: ${format.url}`);
+    });
+    
+    // Display the first image thumbnail
+    // console.log('First Image Thumbnail URL:', info.videoDetails.thumbnails);
+    
+    // formats = [...formats, ...thumbnails]
+    console.log('Available Qualities:',formats);
+    // res.send(formats)
+} catch (error) {
+    console.error('Error fetching video information:', error);
+}
+})
 
 const port = process.env.PORT || 3000;
 
